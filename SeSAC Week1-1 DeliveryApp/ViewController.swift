abb9763797095268b2a936821d82f0192dbbaeb2
roargenie <105975078+roargenie@users.noreply.github.com>
2022-07-08 20:10:14 +0900

    layout

--- SeSAC Week1-1 DeliveryApp/ViewController.swift
+++ SeSAC Week1-1 DeliveryApp/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  SeSAC Week1-1 DeliveryApp
-//
-//  Created by 이명진 on 2022/07/04.
-//
+
 
 import UIKit
 
@@ -19,13 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        print(11)
-        print(22)
-        print(33)
-        
-        print(4)
-        print(5)
-        print(10)
         
         
         
